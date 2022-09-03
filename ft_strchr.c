78705ed107d9f1681b7bd542f224ae45ce422fd2
amrokamalelsiddig <amro.kamal.ak@gmail.com>
2022-09-03 05:38:54 +0400

    fresh libft

---
+++ ft_strchr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelsiddi <aelsiddi@student.42abudhabi.a    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/20 22:06:09 by yidrissi          #+#    #+#             */
+/*   Updated: 2022/09/03 05:30:52 by aelsiddi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char	*s, int c)
+{
+	int		i;
+	char	*s1;
+	int		n;
+
+	i = 0;
+	s1 = (char *)s;
+	n = ft_strlen(s1);
+	while (i <= n)
+	{
+		if (s1[i] == (char)c)
+		{
+			return (&s1[i]);
+		}
+		i++;
+	}
+	return (0);
+}
+//ptr = &s1[i];
